fd15fd41a0453426ceddab6eb012da1596473ea8
Stanislav Konnov <43505907+StanisKo@users.noreply.github.com>
2021-03-18 11:20:32 +0100

    Update routes.ts

--- src/MGME.Web/ClientApp/src/shared/const/routes.ts
+++ src/MGME.Web/ClientApp/src/shared/const/routes.ts
@@ -1,5 +1,11 @@
 const root = '/';
 
+/*
+Rename login into signin
+
+Remove logout, since logout would imply removing the token from sessionStorage
+*/
+
 export const ROUTES = {
     ROOT: `${root}`,
     LOGIN: `${root}login`,
