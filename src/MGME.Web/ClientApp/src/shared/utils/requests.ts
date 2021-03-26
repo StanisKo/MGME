b97dfdcb79690240e2dd0c543cee5c82584038c9
stan <stan@local>
2021-03-26 18:55:31 +0100

    working on network requests and redux

--- src/MGME.Web/ClientApp/src/shared/utils/requests.ts
+++ src/MGME.Web/ClientApp/src/shared/utils/requests.ts
@@ -1,6 +1,5 @@
 // Generalized http function to serve the need of various request methods
-const http = async <T>(request: RequestInfo): Promise<T> =>
-{
+const http = async <T>(request: RequestInfo): Promise<T> => {
     try {
         const response = await fetch(request);
 
@@ -13,25 +12,20 @@ const http = async <T>(request: RequestInfo): Promise<T> =>
     }
 };
 
-// Implementation of HTTP requests by method: GET, POST, PUT.
-export const get = async <T>(url: string): Promise<T> => {
+export const read = async <T>(url: string): Promise<T> => {
     const args: RequestInit = { method: 'GET' };
 
     return await http<T>(new Request(url, args));
 };
 
-export const post = async <T>(
-    url: string, body: { [key: string]: unknown }, headers?: { [key: string]: string }
-): Promise<T> => {
-    const args: RequestInit = { method: 'POST', body: JSON.stringify(body), headers: headers };
+export const write = async <T>(
+    url: string,
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+    body: { [key: string]: unknown },
+    headers?: { [key: string]: string }): Promise<T> => {
+    const args: RequestInit = { method: method, body: JSON.stringify(body), headers: headers };
 
     return await http<T>(new Request(url, args));
 };
 
-export const put = async <T>(
-    url: string, body: { [key: string]: unknown }, headers?: Record<string, string>): Promise<T> => {
-    const args: RequestInit = { method: 'PUT', body: JSON.stringify(body), headers: headers };
-
-    return await http<T>(new Request(url, args));
-};
 
