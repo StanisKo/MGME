4d0e67f76df0fd111a8a48a24a816b9478919724
stan <stan@local>
2021-03-31 18:54:21 +0200

    working on email confirmation

--- src/MGME.Web/ClientApp/src/domain/auth/components/confirmEmail.tsx
+++ src/MGME.Web/ClientApp/src/domain/auth/components/confirmEmail.tsx
@@ -1,15 +1,48 @@
-import { ReactElement } from 'react';
+import React, { ReactElement, useState, SyntheticEvent } from 'react';
 
+import { BaseServiceResponse } from '../../../shared/interfaces';
 import { confirmEmailAddress } from '../requests';
+
+import { Snackbar } from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
 
 import qs from 'qs';
 
 export const ConfirmEmail = (): ReactElement => {
     const token = qs.parse(window.location.search, { ignoreQueryPrefix: true })?.token;
 
+    const [response, setResponse] = useState<BaseServiceResponse>(
+        {} as BaseServiceResponse
+    );
+
+    const [openSnackbar, setOpenSnackbar] = useState<boolean>(false);
+
+    const handleSnackbarClose = (event?: SyntheticEvent, reason?: string): void => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenSnackbar(false);
+    };
+
     if (token) {
-        confirmEmailAddress(token as string);
+        (async (): Promise<void> => {
+            const confirmationResponse = await confirmEmailAddress(token as string);
+
+            if (response.success) {
+                setResponse(confirmationResponse);
+            }
+        })();
     }
 
-    return <div>Thank you for confirming email address</div>;
+    return (
+        <>
+            <div>Thank you for confirming email address</div>
+            <Snackbar open={openSnackbar} onClose={handleSnackbarClose}>
+                <Alert onClose={handleSnackbarClose} severity={response.success ? 'success' : 'warning'}>
+                    {response.message}
+                </Alert>
+            </Snackbar>
+        </>
+    );
 };
