449ce65eb88aa17522d65a6b33923dd5e9ac52c3
stan <stan@local>
2021-04-08 20:47:23 +0200

    keep working on front end

--- src/MGME.Web/ClientApp/src/domain/user/index.tsx
+++ src/MGME.Web/ClientApp/src/domain/user/index.tsx
@@ -6,7 +6,39 @@ import { ApplicationState } from '../../store';
 
 import { getUser, updateUser } from './requests';
 
-import { Button, Paper } from '@material-ui/core';
+import { Button, Paper, Grid, Typography, TextField, IconButton } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import EditIcon from '@material-ui/icons/Edit';
+
+const useStyles = makeStyles((theme) => ({
+    deleteButton: {
+        backgroundColor: '#d32f2f',
+        '&:hover': {
+            backgroundColor: '#b52828'
+        },
+        color: 'white'
+    },
+    centered: {
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    quarterWidth: {
+        width: '25%'
+    },
+    toRight: {
+        display: 'flex',
+        justifyContent: 'flex-end'
+    },
+    editIcon: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        '& > button': {
+            marginLeft: '3em',
+            top: '-0.5em'
+        }
+    }
+}));
 
 export const UserProfile = (): ReactElement | null => {
     const user: User | null = useSelector(
@@ -29,14 +61,88 @@ export const UserProfile = (): ReactElement | null => {
         })();
     }, [isAuthorized]);
 
+    const { centered, deleteButton, quarterWidth, toRight, editIcon } = useStyles();
+
     return user ? (
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div className={centered} >
             <Paper elevation={0}>
-                <div>{`Name: ${user.name}`}</div>
-                <div>{`Email: ${user.email}`}</div>
-                <Button onClick={handleUpdateTest}>
-                    Update
-                </Button>
+                <Grid container spacing={4}>
+                    <Grid item>
+                        <Typography component="h4" variant="h4">
+                            User Profile
+                        </Typography>
+                    </Grid>
+                    <Grid item container xs={11} sm={11} lg={11} direction="column" spacing={4}>
+                        <Grid item>
+                            <Typography component="h6" variant="h6">
+                                {`Name: ${user.name}`}
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography component="h6" variant="h6">
+                                {`Email: ${user.email}`}
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <TextField
+                                className={quarterWidth}
+                                variant="outlined"
+                                required
+                                name="password"
+                                label="Old Password"
+                                type="password"
+                                id="password"
+                                autoComplete="password"
+                                disabled
+                            />
+                        </Grid>
+                        <Grid item>
+                            <TextField
+                                className={quarterWidth}
+                                variant="outlined"
+                                required
+                                name="password"
+                                label="New Password"
+                                type="password"
+                                id="password"
+                                autoComplete="password"
+                                disabled
+                            />
+                        </Grid>
+                        <Grid item>
+                            <TextField
+                                className={quarterWidth}
+                                variant="outlined"
+                                required
+                                name="password"
+                                label="Confirm New Password"
+                                type="password"
+                                id="password"
+                                autoComplete="password"
+                                disabled
+                            />
+                        </Grid>
+                    </Grid>
+
+                    <Grid item xs={1} sm={1} lg={1} className={editIcon}>
+                        <IconButton>
+                            <EditIcon/>
+                        </IconButton>
+                    </Grid>
+
+                    <Grid item container spacing={4} className={toRight}>
+                        <Grid item>
+                            <Button variant="contained" color="primary" onClick={handleUpdateTest} disabled>
+                                Update
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button variant="contained" className={deleteButton}>
+                                Delete
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Paper>
         </div>
     ) : null;
