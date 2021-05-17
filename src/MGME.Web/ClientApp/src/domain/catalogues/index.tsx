d1418ba0f0d7fdc38ef0e7541cdbd2af5f7dad95
stan <stan@local>
2021-05-17 13:55:04 +0200

    menu for catalogues

--- src/MGME.Web/ClientApp/src/domain/catalogues/index.tsx
+++ src/MGME.Web/ClientApp/src/domain/catalogues/index.tsx
@@ -1,11 +1,46 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState, ChangeEvent } from 'react';
 
-import { Box, Typography } from '@material-ui/core';
+import { Paper, Tabs, Tab, Theme } from '@material-ui/core';
 
-export const Catalogues = (): ReactElement => (
-    <Box>
-        <Typography>
-            Catalogues
-        </Typography>
-    </Box>
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        centered: {
+            display: 'flex',
+            justifyContent: 'center'
+        },
+        flexGrow: {
+            flexGrow: 1
+        }
+    })
 );
+
+export const Catalogues = (): ReactElement => {
+
+    const [selectedMenu, setSelectedMenu] = useState<number>(0);
+
+    const handleChange = (event: ChangeEvent<unknown>, newValue: number): void => {
+        setSelectedMenu(newValue);
+    };
+
+    const { centered, flexGrow } = useStyles();
+
+    return (
+        <div className={centered}>
+            <Paper elevation={0}>
+                <Tabs
+                    className={flexGrow}
+                    centered
+                    value={selectedMenu}
+                    onChange={handleChange}
+                    aria-label="menu-tabs"
+                    indicatorColor="primary"
+                >
+                    <Tab label="Player Characters" disableRipple={true}/>
+                    <Tab label="Non Player Characters" disableRipple={true}/>
+                </Tabs>
+            </Paper>
+        </div>
+    );
+};
