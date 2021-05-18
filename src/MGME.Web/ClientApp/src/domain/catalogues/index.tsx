864a40bac6eb3a917432f3eccafdc2376e9c18ce
stan <stan@local>
2021-05-18 13:27:39 +0200

    working on markup

--- src/MGME.Web/ClientApp/src/domain/catalogues/index.tsx
+++ src/MGME.Web/ClientApp/src/domain/catalogues/index.tsx
@@ -2,7 +2,7 @@ import { ReactElement, useState, ChangeEvent } from 'react';
 
 import { PlayerCharactersTable } from './components/playerCharactersTable';
 
-import { Paper, Tabs, Tab, Theme } from '@material-ui/core';
+import { Paper, Grid, Tabs, Tab, Theme } from '@material-ui/core';
 
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 
@@ -30,18 +30,25 @@ export const Catalogues = (): ReactElement => {
     return (
         <div className={centered}>
             <Paper elevation={0}>
-                <Tabs
-                    className={flexGrow}
-                    centered
-                    value={selectedMenu}
-                    onChange={handleChange}
-                    aria-label="menu-tabs"
-                    indicatorColor="primary"
-                >
-                    <Tab label="Player Characters" disableRipple={true}/>
-                    <Tab label="Non Player Characters" disableRipple={true}/>
-                </Tabs>
-                {selectedMenu === 0 && <PlayerCharactersTable />}
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Tabs
+                            className={flexGrow}
+                            centered
+                            value={selectedMenu}
+                            onChange={handleChange}
+                            aria-label="menu-tabs"
+                            indicatorColor="primary"
+                        >
+                            <Tab label="Player Characters" disableRipple={true}/>
+                            <Tab label="Non Player Characters" disableRipple={true}/>
+                        </Tabs>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        {selectedMenu === 0 && <PlayerCharactersTable />}
+                    </Grid>
+                </Grid>
             </Paper>
         </div>
     );
