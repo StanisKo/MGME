5f08857901ac0fd208d1e4031cbc2282905e6124
stan <stan@local>
2021-06-07 17:19:37 +0200

    flattening out dir structure, simplifying

--- src/MGME.Web/ClientApp/src/shared/requests.ts
+++ src/MGME.Web/ClientApp/src/shared/requests.ts
@@ -1,8 +1,8 @@
-import { PaginatedDataServiceResponse, AvailableNonPlayerCharacter } from '../interfaces';
+import { PaginatedDataServiceResponse, AvailableNonPlayerCharacter } from './interfaces';
 
-import { NON_PLAYER_CHARACTER_FILTER } from '../const/enums';
+import { NON_PLAYER_CHARACTER_FILTER } from './const/enums';
 
-import { URLBuilder, DataController } from '../utils';
+import { URLBuilder, DataController } from './utils';
 
 export const fetchAvailableNonPlayerCharacters = async (
     filter: NON_PLAYER_CHARACTER_FILTER,
