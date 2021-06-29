1887a5a7d0ce87be5c711e5a614c01d8b84dd7db
stan <stan@local>
2021-06-29 16:53:17 +0200

    working on request to add to adventure

--- src/MGME.Web/ClientApp/src/shared/requests.ts
+++ src/MGME.Web/ClientApp/src/shared/requests.ts
@@ -1,8 +1,15 @@
-import { PaginatedDataServiceResponse, AvailableNonPlayerCharacter } from './interfaces';
+import { PaginatedDataServiceResponse, AvailableNonPlayerCharacter, BaseServiceResponse } from './interfaces';
 
 import { NON_PLAYER_CHARACTER_FILTER } from './const/enums';
 
 import { URLBuilder, DataController } from './utils';
+
+interface AddToAdventureParams {
+    adventure: number;
+    playerCharacters?: number[];
+    nonPlayerCharacters?: number[];
+    keys: string[];
+}
 
 export const fetchAvailableNonPlayerCharacters = async (
     filter: NON_PLAYER_CHARACTER_FILTER
@@ -18,3 +25,22 @@ export const fetchAvailableNonPlayerCharacters = async (
         }
     ) as PaginatedDataServiceResponse<AvailableNonPlayerCharacter[]>;
 };
+
+export const addToAdventure = async (
+    { adventure, playerCharacters, nonPlayerCharacters, keys }: AddToAdventureParams): Promise<BaseServiceResponse> => {
+
+    return await DataController.UpdateAndRefetch(
+        {
+            url: URLBuilder.WriteTo('adventure', 'addto'),
+            method: 'POST',
+            body: {
+                adventure: adventure,
+                playerCharacters: playerCharacters ?? [],
+                nonPlayerCharacters: nonPlayerCharacters ?? []
+            },
+            page: 'catalogues',
+            keys: keys
+        }
+    );
+};
+
