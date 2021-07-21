432f62aec1e4467c03acf13e94bcee0c0452c3d6
stan <stan@local>
2021-07-21 13:29:28 +0200

    wip

--- src/MGME.Web/Program.cs
+++ src/MGME.Web/Program.cs
@@ -1,10 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
-using Microsoft.EntityFrameworkCore;
-
-using MGME.Infra.Data;
 
 namespace MGME.Web
 {
@@ -12,17 +7,7 @@ namespace MGME.Web
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-
-            // We run migrations on container boot
-            using (IServiceScope scope = host.Services.CreateScope())
-            {
-                ApplicationDbContext context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
-                context.Database.Migrate();
-            }
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
