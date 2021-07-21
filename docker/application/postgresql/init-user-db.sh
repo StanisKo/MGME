94611d8c0edaf72e81b50a342ac67bfb5d500330
stan <stan@local>
2021-07-21 21:09:41 +0200

    simplifying things

--- docker/application/postgresql/init-user-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Create a new database and user and grant all privileges to that user
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DATABASE" <<-EOSQL
-    CREATE USER $SQL_USER WITH PASSWORD '$POSTGRES_PASSWORD';
-    CREATE DATABASE $POSTGRES_DATABASE;
-    GRANT ALL PRIVILEGES ON DATABASE $SQL_DATABASE TO $POSTGRES_USER;
-EOSQL
