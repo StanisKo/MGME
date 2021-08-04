0379dbd1d732be434633374ad17c2145a741f198
stan <stan@local>
2021-08-04 16:20:30 +0200

    raname model fields, move env file parsing outside of Main

--- src/MGME.Core/Entities/RandomEvent.cs
+++ src/MGME.Core/Entities/RandomEvent.cs
@@ -6,10 +6,7 @@ namespace MGME.Core.Entities
     public class RandomEvent : BaseEntity
     {
         [Required]
-        public string Focus { get; set; }
-
-        [Required]
-        public string Meaning { get; set; }
+        public string Description { get; set; }
 
         public string Interpetation { get; set; }
 
--- src/MGME.Web/Program.cs
+++ src/MGME.Web/Program.cs
@@ -17,18 +17,6 @@ namespace MGME.Web
     {
         public static void Main(string[] args)
         {
-            /*
-            Environment file parsing is necessary only for local development
-
-            Since we cannot point asp to .env file as we can via docker-compose,
-            We need to pin down the file and load the variables into memory programmatically
-            */
-            string rootDirectory = Directory.GetCurrentDirectory();
-
-            string environmentFile = Path.Combine(rootDirectory, "../../.env");
-
-            DotEnv.Load(environmentFile);
-
             IHost host = CreateHostBuilder(args).Build();
 
             using (IServiceScope scope = host.Services.CreateScope())
@@ -57,9 +45,21 @@ namespace MGME.Web
             host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            /*
+            Environment file parsing is necessary only for local development
 
-            Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((_, config) =>
+            Since we cannot point asp to .env file as we can via docker-compose,
+            We need to pin down the file and load the variables into memory programmatically
+            */
+            string rootDirectory = Directory.GetCurrentDirectory();
+
+            string environmentFile = Path.Combine(rootDirectory, "../../.env");
+
+            DotEnv.Load(environmentFile);
+
+            return Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((_, config) =>
                 {
                     config.AddEnvironmentVariables();
                 }
@@ -68,5 +68,6 @@ namespace MGME.Web
                     webBuilder.UseStartup<Startup>();
                 }
             );
+        }
     }
 }
