24a3f9dbdeb47681b561aa74fbad3cc1e1d2992d
stan <stan@local>
2021-08-10 20:57:13 +0200

    update scene item

---
+++ src/MGME.Infra/Migrations/20210810185545_FixTypoOnColumnName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MGME.Infra.Migrations
+{
+    public partial class FixTypoOnColumnName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Interpetation",
+                table: "RandomEvents",
+                newName: "Interpretation");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Interpretation",
+                table: "RandomEvents",
+                newName: "Interpetation");
+        }
+    }
+}
